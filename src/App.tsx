fa28709b02d8061667f3883f316229372224e6f3
Suren <sgkochar@gmail.com>
2022-10-25 16:04:43 +0300

    add social-search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import Header from './Components/Header/Header';
-import './app.css'
-
-
+import React from "react";
+import Header from "./Components/Header/Header";
+import "./app.css";
+import Slider from "./Components/Slider/Slider";
 
 function App() {
   return (
-    <Header/>
+    <>
+      <Header />
+      <Slider />
+    </>
   );
 }
 
---
+++ src/Components/Slider/Slider.tsx
@@ -0,0 +1,17 @@
+import react from 'react'
+
+const sliderStyle = {
+    backgroundColor: '#000',
+    width: '100%',
+    height: 711,
+    padding: '1.5em',
+    fontSize: 42,
+    color: 'white',
+ }
+
+const Slider = () => {
+    return (
+        <div style={sliderStyle }>Slider</div>
+    )
+}
+export default Slider
