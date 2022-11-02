c455da060576d00163b512df1d7c850fdda24f35
Suren <sgkochar@gmail.com>
2022-11-02 20:17:39 +0200

    add swiper slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Header from "./Components/Header/Header";
 import "./app.css";
-import Slider from "./Components/Slider/Slider";
 import Main from "./Components/Main/Main"
 import Footer from "./Components/Footer/Footer";
+import Slider from "./Components/Slider/Slider";
 
 function App() {
   return (
--- src/Components/Slider/Slider.tsx
+++ src/Components/Slider/Slider.tsx
@@ -1,17 +1,26 @@
-import react from 'react'
-
-const sliderStyle = {
-    backgroundColor: '#000',
-    width: '100%',
-    height: 200,
-    padding: '1.5em',
-    fontSize: 42,
-    color: 'white',
- }
+import react from "react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "./slider.css";
 
 const Slider = () => {
-    return (
-        <div style={sliderStyle }>Slider</div>
-    )
-}
-export default Slider
+  return (
+    <div className="slider">
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide>
+            <img src="./images/macbook.png"></img>
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src="./images/macbook.png"></img>
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src="./images/macbook.png"></img>
+        </SwiperSlide>
+    
+      </Swiper>
+    </div>
+  );
+};
+export default Slider;
